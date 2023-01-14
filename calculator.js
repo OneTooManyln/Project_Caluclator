6d29a2338409b2e09fd00585c657d6502c1799b3
OneTooMany <santosismael.115@gmail.com>
2023-01-14 15:56:14 -0800

    Created functins for bassic required basic math operators

---
+++ calculator.js
@@ -0,0 +1,16 @@
+function add(a, b) {
+    return a + b;
+} console.log(add(5, 5));
+
+
+function subtract(a, b) {
+    return a - b ;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    return a / b;
+}
