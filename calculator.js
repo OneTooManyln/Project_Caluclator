9124ad24da673e2888f98d3f7ab314a46a04d621
OneTooMany <santosismael.115@gmail.com>
2023-01-20 14:03:44 -0800

    Add green notes outlining a different approach to make calculator work. operation will no longer be called when an operator button is clicked instead when the equals button is clicked a new function should be called

--- calculator.js
+++ calculator.js
@@ -34,18 +34,11 @@ const numBtn = document.querySelectorAll('button[data-number]');
 const btn = document.querySelectorAll('button');
 const display = document.querySelector('.screen-display');
 const result = document.querySelector('.screen-display');
+const opBtn = document.querySelectorAll('button[data-operator]');
 
 numBtn.forEach((numBtn) => {
     numBtn.addEventListener('click', () => populate(numBtn.getAttribute('data-number')))
 })
-
-function populate(n) {
-    if(display.textContent == '0') 
-        display.textContent = '';
-        display.textContent += n;    
-}
-
-const opBtn = document.querySelectorAll('button[data-operator]');
 
 opBtn.forEach((opBtn) => {
     opBtn.addEventListener('click', () => { 
@@ -53,9 +46,21 @@ opBtn.forEach((opBtn) => {
         display.textContent = ''
         display.textContent += ' ' + opBtn.textContent + ' ';
         
-        operation(opBtn.getAttribute('data-operator'))
+        //operation(opBtn.getAttribute('data-operator'))
     })
 })
+
+//Instead of calling the function operation when a operator is clicked, it should instead be called when
+//the equal's button is clicked.
+//When clicked, a function will split the operation into three seperate value:
+//  firstOperand, operator, and secondOperand
+//the function evaluateOperation would then be called using the new values defined above.
+
+function populate(n) {
+    if(display.textContent == '0') 
+        display.textContent = '';
+        display.textContent += n;    
+}
 
 function operation(oper) {
     firstOperand = display.textContent;
